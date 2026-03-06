6a6d6bafecb502618b78efac673d3ccb58b28e97
Arif <mdarifhosain214@gmail.com>
2026-03-06 16:20:54 +0600

    updated

---
+++ lib/core/route/route_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../presentation/bottom_nav/view/bottom_nav_bar_screen.dart';
+import '../../presentation/splash/view/splash_screen.dart';
+import '../resource/strings_manager.dart';
+import 'routes_name.dart';
+
+
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RoutesName.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case RoutesName.bottomNavRoute:
+        return MaterialPageRoute(builder: (_) => BottomNavBarScreen());
+
+      default:
+        return unDefineRoute();
+    }
+  }
+
+  static Route<dynamic> unDefineRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: Text(StringManager.noRoute)),
+        body: Center(child: Text(StringManager.noRoute)),
+      ),
+    );
+  }
+}
